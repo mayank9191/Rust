1a84d6cc53d32134bc65161f0a034c22e0376a61
Mayank Kulahara <mayankkulahara@gmail.com>
2024-12-27 14:01:32 +0530

    first commit

---
+++ hello_world/1.rs
@@ -0,0 +1,14 @@
+fn main(){
+  println!("Hello World");
+}
+
+
+//1)Created a folder Rust.
+//2)Created a file 1.rs.
+//3)To run the created file either use code runner or run command in terminal (rustc file_name.rs) -> then (.\file_name)
+//4)In terminal (Commands steps wises) rustc 1.rs -> .\1
+//5)cargo --version.
+//6)cargo new hello_world
+//7)cd hello_world folder
+//8)cargo build
+//9)cargo run
